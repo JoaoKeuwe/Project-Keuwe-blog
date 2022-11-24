4c9241cd1a1df5b52239fd2531ad36e3381fb39a
joaokeuwe <joaov2872@hotmail.com>
2022-11-23 21:57:36 -0300

    feat: :fire: Adicionando foto da empresa ao Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Logo from '../images/logoBtix.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav>
+      <img width={150} src={Logo} alt="Logo da empresa Btix" />
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
   )
 }
 
