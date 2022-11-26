ce809420ef723c336d6c0eaa271b4f2dc8ed8e3f
joaokeuwe <joaov2872@hotmail.com>
2022-11-25 22:14:40 -0300

    refactor: :ambulance: Refatorando o codigo que antes era de teste, e iniciando um codigo mais limpo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from 'react-router-dom';
+import PostCard from '../components/PostCard';
+import pessoa1 from '../images/pessoa1.jpg'
+// import {Link} from "react-router-dom"
+
+function Home() {
+  const [repositories, setRepositories] = useState()
+  const [handleRepositories] = useState()
+  const [pagination, setPagination] = useState({ page: 1, content: [] })
+
+  const state = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data);
+        setPagination({ page: 1, content: data.slice(0, 10) })
+      })
+  }
+  useEffect(() => {
+    state()
+  }, [])
+
+  useEffect(() => {
+  }, [handleRepositories])
+
+  const nextPage = () => {
+    if (pagination.page !== 9) {
+      setPagination((prev) => ({ page: prev.page + 1, content: repositories.slice((prev.page + 1) * 10, 10 * (prev.page + 2)) }))
+    }
+  }
+
+  const previousPage = () => {
+    if (pagination.page !== 1) {
+      setPagination((prev) => ({ page: prev.page - 1, content: repositories.slice(10 * (prev.page - 2), (prev.page - 1) * 10) }))
+    }
+  }
+
+  //  logica pra pegar os comentarios 
+
+  //   <button  onClick={async() => {
+  //     const comentarios = await fetch(`https://jsonplaceholder.typicode.com/posts/${id.id}/comments`)
+  //     setPostComentarios(comentarios);
+  //     console.log(await comentarios.json());
+  //   }}>teste</button>
+
+  return (
+    <section>
+      <img width={100} src={pessoa1} alt="" />
+      <h1>Blog Btix</h1>
+      {pagination.content.map((post) => (
+        <PostCard
+          id={post.id}
+          title={post.title}
+          body={post.body}
+        />
+      ))}
+ 
+      <button onClick={() => nextPage()}>proxima pag</button>
+      <button onClick={() => previousPage()}>voltar</button>
+    </section >
+  )
+}
+
+export default Home
