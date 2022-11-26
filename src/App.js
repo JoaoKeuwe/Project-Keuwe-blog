ac3dd0abb35c4f7dfead4cefcf7e4013b6ecec94
joaokeuwe <joaov2872@hotmail.com>
2022-11-25 22:49:38 -0300

    feat: :fire: Renderizando componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-// import Header from './components/Header'
+import Header from './components/Header'
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
