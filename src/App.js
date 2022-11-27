d1e28c4ee23c414d6bee1203eba34da7486c20cc
joaokeuwe <joaov2872@hotmail.com>
2022-11-26 22:41:53 -0300

    fix: :bug: Retirando importação de Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Header from './components/Header'
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Home />
     </div>
   );
