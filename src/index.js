1778937ffefad63d12eec0d399ea326c97c890e1
joaokeuwe <joaov2872@hotmail.com>
2022-11-26 22:42:11 -0300

    fix: :bug: Removendo comentários

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
+    {/* <Route exact path="/comments/:id" component={Comments} /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
