4fb9f2293271f22bf197cf7a4b0f3cdb122300fc
joaokeuwe <joaov2872@hotmail.com>
2022-11-26 22:43:07 -0300

    feat: :fire: Lógica que altera a rota   leva para a pagina de comentários(incompleto)

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function PostCard({id, title, body}) {
   return (
-    <main>
-    <p>{id}</p>
+    <main> 
+   <Link to={`/comments/${id}`}> <p>{id}</p></Link>
     <h2>{title}</h2>
     <p>{body}</p>
     <hr />
