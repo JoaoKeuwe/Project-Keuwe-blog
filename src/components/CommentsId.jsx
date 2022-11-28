7e917e7cb79921373c3f74a5ddb6ba5afb15b651
joaokeuwe <joaov2872@hotmail.com>
2022-11-28 16:46:37 -0300

    feat: :fire: Criando componente CommentsId

---
+++ src/components/CommentsId.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+import Hero from './hero'
+
+function CommentsId() {
+  const { id } = useParams()
+  const [commentsId, setCommentsId] = useState([])
+
+  const state = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      .then(response => {
+        response.json()
+          .then(data => {
+            setCommentsId(data)
+          });
+      })
+  }
+
+  useEffect(() => {
+    state()
+  })
+
+
+  return (
+    <div>
+      <Hero />
+      {commentsId.map((commentsId) => (
+        <main>
+          <h2>{commentsId.name}</h2>
+          <p>{commentsId.body}</p>
+          <hr />
+        </main>
+      ))}
+    </div>
+  )
+}
+
+export default CommentsId
