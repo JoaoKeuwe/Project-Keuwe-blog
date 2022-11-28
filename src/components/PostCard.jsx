d513c2b90ac822026e830611df398e3ff4bb3e7a
joaokeuwe <joaov2872@hotmail.com>
2022-11-27 21:17:14 -0300

    feat: :fire: passando props no component PostCrad.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
-function PostCard({id, title, body}) {
+function PostCard({ id, title, body }) {
   return (
-    <main> 
-   <Link to={`/comments/${id}`}> <p>{id}</p></Link>
-    <h2>{title}</h2>
-    <p>{body}</p>
-    <hr />
-  </main>
+    <main>
+      <Link to={`/comments/${id}`}> <p>{id}</p></Link>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <hr />
+    </main>
   )
 }
 
