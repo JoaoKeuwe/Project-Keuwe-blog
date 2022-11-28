71bd76030d292a01ebcbd8f14f4949f7ecb727f5
joaokeuwe <joaov2872@hotmail.com>
2022-11-28 01:44:44 -0300

    feat: :fire: Criando componente Comments

---
+++ src/components/Comments.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useEffect, useState } from "react"
+
+function Comments() {
+  const [comments, setComments] = useState([])
+
+  const fetch_url = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+      .then(response => response.json())
+      .then(data => {
+        setComments(data)
+      })
+  }
+
+  useEffect(() => {
+    fetch_url()
+  }, [])
+
+  return (
+    <div>
+      {comments.map((comments) => (
+        <main>
+          <p>{comments.id}</p>
+          <p>{comments.body}</p>
+          <p>{comments.name}</p>
+        </main>
+      ))}</div>
+  )
+}
+
+export default Comments
