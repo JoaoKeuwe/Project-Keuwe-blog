491c849e5b347334a29024cd1a9947cf56739003
joaokeuwe <joaov2872@hotmail.com>
2022-11-28 03:43:55 -0300

    feat: :fire: Adicionando importações do hero e do reset css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from '../components/Header'
+import Header from '../components/hero'
 import Home from '../components/Home';
+import '../styles/Reset.css'
 
 function HomePage() {
 
