7d13f93b0a398850b931bd2207118e56d93da1e6
joaokeuwe <joaov2872@hotmail.com>
2022-11-28 01:16:00 -0300

    feat: :fire: Renderizando apenas componentes na pagina Home

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header'
+import Home from '../components/Home';
+
+function HomePage() {
+
+  return (
+    <section>
+      <Header />
+      <h1>Blog Btix</h1>
+      <Home />
+    </section >
+  )
+}
+
+export default HomePage
