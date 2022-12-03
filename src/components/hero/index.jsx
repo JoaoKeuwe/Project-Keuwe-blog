0d85ce5922bd12036f8aa1989a81fd686fdec183
JoaoKeuwe <joaov2872@hotmail.com>
2022-12-03 14:31:52 -0300

    refactor: :bug: alterando imagem do hero

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router'
-import Logo from '../../images/logo.webp'
+import Logo from '../../images/logo.png'
 import * as S from './style'
 
 export function Hero() {
@@ -16,13 +16,13 @@ export function Hero() {
   return (
     <S.HeaderHero>
       <S.NavMenu>
-        <img width={120} src={Logo} alt="Logo da empresa Btix" />
+    
         <S.NavOptions>
           <a href='/'>Posts</a>
         </S.NavOptions>
       </S.NavMenu>
       <div>
-        <h1>Blog Btix</h1>
+      <img width={400} src={Logo} alt="Logo" />
         <h2>{subTitle()}</h2>
       </div>
     </S.HeaderHero>
